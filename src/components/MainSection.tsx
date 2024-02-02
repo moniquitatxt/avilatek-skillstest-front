553bab3349e9d821c22b61475cc3ca7b8620b98d
M <monacuaulma@gmail.com>
2024-02-02 09:35:05 -0400

    Add plus and minus icons

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -3,7 +3,7 @@ import PurpleButton from "./PurpleButton";
 
 const MainSection: React.FC = () => {
 	return (
-		<div className="h-[1100px] ">
+		<div className="md:h-[1100px] sm:h-[900px] ">
 			<div className=" bg-purple-500 text-white  h-[674px] md:rounded-3xl pt-[96px] pb-[192px] p-x-[96px] text-center ">
 				<div>
 					<h1 className=" md:text-[72px] md:leading-[90px] text-[36px] leading-[44px] font-semibold  tracking-wide  ">
@@ -22,7 +22,7 @@ const MainSection: React.FC = () => {
 						<input
 							type="text"
 							placeholder="Enter your email"
-							className="p-2 w-[335px] h-[48px]  border-white rounded-md focus:outline-none"
+							className="p-2 w-[335px] h-[48px]  border-white text-black rounded-md focus:outline-none"
 						/>
 						<p className="text-[14px] leading-[20px] my-[6px] text-[#E9D7FE]">
 							We care about your data in our{" "}
@@ -40,7 +40,7 @@ const MainSection: React.FC = () => {
 					width={800}
 					height={492}
 					alt="graphic"
-					className="absolute  left-1/2 transform -translate-x-1/2 bottom-[-520px] z-0"
+					className="absolute left-1/2 transform -translate-x-1/2 md:bottom-[-520px]  z-0"
 				/>
 			</div>
 		</div>
