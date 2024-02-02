5c237c2e4e8b0ea55d03e1ca20a53e40325cb455
M <monacuaulma@gmail.com>
2024-02-02 07:03:53 -0400

    Responsive main section

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -4,27 +4,27 @@ import PurpleButton from "./PurpleButton";
 const MainSection: React.FC = () => {
 	return (
 		<div className="h-[1100px] ">
-			<div className=" bg-purple-500 text-white  h-[674px] rounded-3xl pt-[96px] pb-[192px] p-x-[96px] text-center ">
+			<div className=" bg-purple-500 text-white  h-[674px] md:rounded-3xl pt-[96px] pb-[192px] p-x-[96px] text-center ">
 				<div>
-					<h1 className=" text-[72px]  font-semibold leading-[90px] tracking-wide">
+					<h1 className=" md:text-[72px] md:leading-[90px] text-[36px] leading-[44px] font-semibold  tracking-wide  ">
 						Grow your users.
 						<br /> Smarter.
 					</h1>
 
-					<p className=" max-w-[768px] text-[#E9D7FE] mx-auto  leading-[30px] font-normal text-[20px] m-[24px] ">
+					<p className="md:text-[20px] md:leading-[30px] md:max-w-[768px] text-[18px] leading-[28px] max-w-[343px] mx-auto text-[#E9D7FE] font-normal m-[24px] ">
 						Powerful, self-serve product and growth analytics to help you
 						convert, engage, and retain more users. Trusted by over 4,000
 						startups.
 					</p>
 				</div>
-				<div className="flex justify-between mx-auto max-w-[480px] m-[16px]">
+				<div className="md:flex justify-between mx-auto max-w-[480px] m-[16px]">
 					<div className="">
 						<input
 							type="text"
 							placeholder="Enter your email"
-							className="p-2 w-[335px]  h-[48px]  border-white rounded-md focus:outline-none"
+							className="p-2 w-[335px] h-[48px]  border-white rounded-md focus:outline-none"
 						/>
-						<p className="text-[14px] leading-[20px] text-[#E9D7FE]">
+						<p className="text-[14px] leading-[20px] my-[6px] text-[#E9D7FE]">
 							We care about your data in our{" "}
 							<a href="/privacy-policy" className="underline">
 								privacy policy
--- src/components/PurpleButton.tsx
+++ src/components/PurpleButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PurpleButton = () => {
 	return (
-		<button className="bg-purple-300 w-[129px] h-[48px]  text-white  p-y-[12px] p-x-[18px] rounded-[8px] ">
+		<button className="bg-purple-300 text-[16px] leading-[24px] md:w-[129px] h-[48px] max-w-[343px] w-full text-white my-[6px] p-y-[12px] p-x-[18px] rounded-[8px] ">
 			Get Started
 		</button>
 	);
